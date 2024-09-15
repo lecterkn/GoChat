ae4d38896602efd2ee64e8d8a4443f0982fe7660
lecter <lecterkn@proton.me>
2024-09-16 00:54:32 +0900

    エラーレスポンスを改修

--- common/ErrorResponse.go
+++ common/ErrorResponse.go
@@ -1,27 +1,33 @@
 package common
 
+import "net/http"
+
+// エラー時のレスポンスボディ
 type ErrorResponse struct {
 	Code string `json:"code"`
 	Message string `json:"message"`
 }
 
-func ValidationErrorResponse(message string) ErrorResponse {
-	return ErrorResponse{
-		"400",
-		message,
-	}
+func ValidationErrorResponse(message string) (int, ErrorResponse) {
+	return http.StatusBadRequest, 
+		ErrorResponse{
+			"400",
+			message,
+		}
 }
 
-func InternalErrorResponse(message string) ErrorResponse {
-	return ErrorResponse{
-		"500",
-		message,
-	}
+func InternalErrorResponse(message string) (int, ErrorResponse) {
+	return http.StatusInternalServerError,
+		ErrorResponse{
+			"500",
+			message,
+		}
 }
 
-func NotFoundErrorResponse(message string) ErrorResponse {
-	return ErrorResponse{
-		"404",
-		message,
-	}
+func NotFoundErrorResponse(message string) (int, ErrorResponse) {
+	return http.StatusNotFound,
+		ErrorResponse{
+			"404",
+			message,
+		}
 }
